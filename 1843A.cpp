49d5a2e37761477effad2a7ce65571f4317410f0
Naranbat <massive0304@gmail.com>
2023-06-24 14:50:23 +0800

    feat: 1843A

---
+++ 1843A.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define PI 3.14159
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        sort(a, a + n);
+
+        int l = 0, r = n - 1;
+
+        int ans = 0;
+
+        while (l < r)
+        {
+            ans += a[r] - a[l];
+            l++;
+            r--;
+        }
+
+        cout << ans << "\n";
+    }
+}
+
+/*
+N - ee shalga
+Never Give Up
+Сая сая мөрөөдөл минь удахгүй нэг нэгээрэ биелэж эхлэх болно.
+
+Whatever our souls also made of, hers and mine are the same.
+
+People aren’t prison of the fate, they’re prison of thier own mind.
+*/
